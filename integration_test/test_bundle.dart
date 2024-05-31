e80594ec7a6160b5571487ed9367040d9c2a5abf
rensawamo <ren0112ngzk@gmail.com>
2024-05-31 11:41:01 +0900

    fix: java ver → 17

--- integration_test/test_bundle.dart
+++ integration_test/test_bundle.dart
@@ -9,7 +9,7 @@ import 'package:patrol/src/native/contracts/contracts.dart';
 import 'package:test_api/src/backend/invoker.dart';
 
 // START: GENERATED TEST IMPORTS
-import 'demo_test.dart' as demo_test;
+import 'example_test.dart' as example_test;
 // END: GENERATED TEST IMPORTS
 
 Future<void> main() async {
@@ -66,7 +66,7 @@ Future<void> main() async {
   });
 
   // START: GENERATED TEST GROUPS
-  group('demo_test', demo_test.main);
+  group('example_test', example_test.main);
   // END: GENERATED TEST GROUPS
 
   final dartTestGroup = await testExplorationCompleter.future;
