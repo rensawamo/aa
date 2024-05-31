115f24d1450eb8f2b1193c445977b7ebb0842102
rensawamo <ren0112ngzk@gmail.com>
2024-05-31 15:56:21 +0900

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,96 @@
+import 'package:animations/animations.dart';
+import 'package:example/cubit/auth_cubit.dart';
+import 'package:example/firebase_options.dart';
+import 'package:example/handlers/notification_handler.dart';
+import 'package:example/pages/home_page.dart';
+import 'package:example/pages/push_notification/notification_success_page.dart';
+import 'package:example/ui/style/colors.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(MyApp());
+const firebaseEnabled = String.fromEnvironment('FIREBASE_ENABLED') == 'true';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initApp();
+  debugPrint('!Google sign in will not work when dev flavor used!');
+
+  runApp(const MyApp());
 }
+
+Future<void> initApp() async {
+  _setUpTheme();
+
+  if (firebaseEnabled) {
+    await _initFirebase();
+    await _askForLocationPermission();
+  }
+}
+
+void _setUpTheme() {
+  final mySystemTheme = SystemUiOverlayStyle.light
+      .copyWith(systemNavigationBarColor: PTColors.lcBlack);
+
+  SystemChrome.setSystemUIOverlayStyle(mySystemTheme);
+}
+
+Future<void> _initFirebase() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+}
+
+Future<void> _askForLocationPermission() => Geolocator.requestPermission();
 
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: CounterPage(),
-    );
-  }
-}
-
-class CounterPage extends StatefulWidget {
-  @override
-  _CounterPageState createState() => _CounterPageState();
-}
-
-class _CounterPageState extends State<CounterPage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('app'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return BlocProvider<AuthCubit>(
+      create: (context) => AuthCubit(
+        firebaseEnabled ? FirebaseAuth.instance : null,
+        firebaseEnabled ? GoogleSignIn() : null,
+      )..init(),
+      child: Provider(
+        lazy: false,
+        create: (_) => NotificationHandler(
+          FlutterLocalNotificationsPlugin(),
+          firebaseEnabled ? FirebaseMessaging.instance : null,
+        )..init(() => Navigator.push(context, notificationRoute)),
+        child: MaterialApp(
+          theme: ThemeData.dark().copyWith(
+            pageTransitionsTheme: const PageTransitionsTheme(
+              builders: {
+                TargetPlatform.android: SharedAxisPageTransitionsBuilder(
+                  transitionType: SharedAxisTransitionType.horizontal,
+                ),
+                TargetPlatform.iOS: SharedAxisPageTransitionsBuilder(
+                  transitionType: SharedAxisTransitionType.horizontal,
+                ),
+              },
             ),
-            Text(
-              '$_counter',
+            colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.grey),
+            primaryColor: PTColors.lcBlack,
+            canvasColor: PTColors.textDark,
+            textSelectionTheme: TextSelectionThemeData(
+              selectionColor: PTColors.lcYellow.withOpacity(0.5),
+              cursorColor: PTColors.textWhite,
+              selectionHandleColor: PTColors.lcYellow,
             ),
-          ],
+          ),
+          debugShowCheckedModeBanner: false,
+          title: 'Patrol Challenge',
+          home: const HomePage(),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
